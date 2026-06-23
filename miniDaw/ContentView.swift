368793c8ac46ab8bae2d311fdd959c7b9a73c2cf
Artur Makoev <arturmakoev@gmail.com>
2026-06-23 14:58:32 +0200

    Initial Commit

---
+++ miniDaw/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  miniDaw
+//
+//  Created by Artur Makoev on 23.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ miniDaw/miniDawApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniDawApp.swift
+//  miniDaw
+//
+//  Created by Artur Makoev on 23.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct miniDawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
